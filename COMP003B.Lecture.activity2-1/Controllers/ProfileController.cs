eee36145d4c1aed96539854595cbfaaa99df6493
romanvillagran <romanvillagran386@gmail.com>
2025-03-24 18:41:50 -0700

    added to profile controller

---
+++ COMP003B.Lecture.activity2-1/Controllers/ProfileController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Lecture.activity2_1.Controllers
+{
+    public class ProfileController : Controller
+    {
+        [HttpGet]
+        public IActionResult About()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Hobbies()
+        {
+            return View();
+        }
+    }
+}
