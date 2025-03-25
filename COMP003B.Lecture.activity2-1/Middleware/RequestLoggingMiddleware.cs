d4adbc39f97f9059a407896f15c20746e1691c9d
romanvillagran <romanvillagran386@gmail.com>
2025-03-24 18:34:38 -0700

    added everything in the about.cshtml

---
+++ COMP003B.Lecture.activity2-1/Middleware/RequestLoggingMiddleware.cs
@@ -0,0 +1,20 @@
+﻿namespace COMP003B.Lecture.activity2_1.Middleware
+{
+    public class RequestLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+
+        public RequestLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Console.WriteLine($"[Request] {context.Request.Method} {context.Request.Path}");
+            await _next(context);
+            Console.WriteLine($"[Response] {context.Response.StatusCode}");
+        }
+    }
+}
