50cb486af03b9966d218947cfaa8bd05e7fb6916
romanvillagran <romanvillagran386@gmail.com>
2025-03-24 18:38:35 -0700

    added the other half of the profile folder

---
+++ COMP003B.Lecture.activity2-1/Views/Profile/Hobbies.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace COMP003B.Lecture.activity2_1.Views.Profile
+{
+    public class HobbiesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
