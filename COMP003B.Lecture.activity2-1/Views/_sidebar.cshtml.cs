22864be9fab88ee618d3c027525e44a45cf63f02
romanvillagran <romanvillagran386@gmail.com>
2025-03-24 19:04:45 -0700

    added the rest

---
+++ COMP003B.Lecture.activity2-1/Views/_sidebar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace COMP003B.Lecture.activity2_1.Views
+{
+    public class _sidebarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
